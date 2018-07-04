4889bfcc956dfecc67290326f2d5598c993bbb41
Pepijn de Vos <pepijndevos@gmail.com>
2018-07-04 17:46:33 +0200

    add PyOpenCL compat class

---
+++ futhark_ffi/compat.py
@@ -0,0 +1,45 @@
+from futhark_ffi import Futhark
+from functools import partial, wraps
+
+class Gettable(object):
+    def __init__(self, fh, futdata):
+        self.fh = fh
+        self.futdata = futdata
+
+    def get(self):
+        return self.fh.from_futhark(self.futdata)
+
+class FutharkCompat(Futhark):
+    """
+    A subclass that wraps all arrays in
+    a class with a `get` method.
+    For compatability with PyOpenCL
+    """
+
+    def make_wrapper(self, ff):
+        wrapper = Futhark.make_wrapper(self,ff)
+        
+        @wraps(wrapper)
+        def subwrapper(*args):
+            uwargs = []
+            for arg in args:
+                if hasattr(arg, 'futdata'):
+                    uwargs.append(arg.futdata)
+                else:
+                    uwargs.append(arg)
+            res = wrapper(*uwargs)
+            wrres = []
+            try:
+                for r in res:
+                    if isinstance(r, self.ffi.CData):
+                        wrres.append(Gettable(self, r))
+                    else:
+                        wrres.append(r)
+                return tuple(wrres)
+            except TypeError:
+                if isinstance(res, self.ffi.CData):
+                    return Gettable(self, res)
+                else:
+                    return res
+
+        return subwrapper
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from numpy.random import random
 from numpy.testing import assert_array_equal
 import _test
 from futhark_ffi import Futhark
+from futhark_ffi.compat import FutharkCompat
 
 class TestFFI(unittest.TestCase):
     def setUp(self):
@@ -40,6 +41,37 @@ class TestFFI(unittest.TestCase):
         assert_array_equal(pos, np.arange(10))
         assert_array_equal(neg, -np.arange(10))
         
+class TestCompat(unittest.TestCase):
+    def setUp(self):
+        self.fut = FutharkCompat(_test)
+
+    def test_int(self):
+        self.assertEqual(self.fut.test1(5), 6)
+
+    def test_list_input(self):
+        data = np.arange(10)
+        self.assertEqual(self.fut.test2(data), np.sum(data))
+
+    def test_list_output(self):
+        data = np.arange(10)
+        res = self.fut.test3(data).get()
+        assert_array_equal(res, np.cumsum(data))
+
+    def test_multi_output(self):
+        self.assertEqual(self.fut.test4(4,5), (4+5, 4-5))
+
+    def test_2d(self):
+        data = np.arange(9).reshape(3,3)
+        res = self.fut.test5(data).get()
+        assert_array_equal(res, data*2)
+        with self.assertRaises(ValueError):
+            self.fut.test5(data.T)
+
+    def test_opaque(self):
+        res = self.fut.test6(10)
+        (pos, neg) = self.fut.test7(res)
+        assert_array_equal(pos.get(), np.arange(10))
+        assert_array_equal(neg.get(), -np.arange(10))
 
 if __name__ == '__main__':
     unittest.main()
