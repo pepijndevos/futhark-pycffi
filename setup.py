5d6a40a2fa9d832075445c6c264354a3b12a0698
Pepijn de Vos <pepijndevos@gmail.com>
2018-07-04 17:59:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='futhark_ffi',
-    version='0.6.1',
+    version='0.6.1b1',
     description='A Python library using the Futhark C backend via CFFI ',
     long_description=long_description,
     long_description_content_type='text/markdown',
