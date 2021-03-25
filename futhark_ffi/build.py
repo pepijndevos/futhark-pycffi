95118b91e0abda927d5040fd0404764045598363
Troels Henriksen <athas@sigkill.dk>
2021-03-25 22:06:11 +0100

    Better handling of backends.

--- futhark_ffi/build.py
+++ futhark_ffi/build.py
@@ -10,18 +10,32 @@ def strip_includes(header):
 def build(name):
     ffibuilder = FFI()
 
-    with open(name+'.c') as source:
-        if sys.platform == 'darwin':
-            libraries=['m']
-            extra_compile_args = ['-std=c99', '-framework', 'OpenCL']
-        else:
-            libraries=['OpenCL']
-            extra_compile_args = ['-std=c99']
+    header_file = name+'.h'
+    source_file = name+'.c'
+
+    search = re.search('#define FUTHARK_BACKEND_([a-z0-9_]*)', open(header_file).read())
+    if not search:
+        sys.exit('Cannot determine Futhark backend from {}'.format(header_file))
+
+    backend=search.group(1)
+    print(backend)
+
+    with open(source_file) as source:
+        libraries = ['m']
+        extra_compile_args = ['-std=c99']
+        if backend == 'opencl':
+            if sys.platform == 'darwin':
+                extra_compile_args += ['-framework', 'OpenCL']
+            libraries += ['OpenCL']
+        elif backend == 'cuda':
+            libraries += ['cuda', 'cudart', 'nvrtc']
+        elif backend == 'multicore':
+            extra_compile_args += ['-pthread']
         ffibuilder.set_source('_'+name, source.read(),
                               libraries=libraries,
                               extra_compile_args=extra_compile_args)
 
-    with open(name+'.h') as header:
+    with open(header_file) as header:
         cdef = 'typedef void* cl_command_queue;'
         cdef += '\ntypedef void* cl_mem;'
         cdef += strip_includes(header.read())
