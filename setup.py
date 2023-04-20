44fba6d235321e8347e0904eea1a9203ec01bef0
Troels Henriksen <athas@sigkill.dk>
2023-04-20 20:19:00 +0200

    This is 0.15.0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='futhark_ffi',
-    version='0.14.2',
+    version='0.15.0',
     description='A Python library using the Futhark C backend via CFFI ',
     long_description=long_description,
     long_description_content_type='text/markdown',
